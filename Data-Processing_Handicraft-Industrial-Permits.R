b5b5989c26e8d197b1fcb39a21dd0718638959bf
gbaltor <baltorgabriel@gmail.com>
2022-03-13 21:54:26 +1100

    s

--- Data-Processing_Handicraft-Industrial-Permits.R
+++ Data-Processing_Handicraft-Industrial-Permits.R
@@ -1,4 +1,12 @@
 # Libraries ####
 library(tidyverse)
+install.packages("readxl")
+library("readxl")
 
-# Data Extraction ####
+# Data Processing ####
+
+# Extraction: Read Excel
+handicraft_industrial_permits <- read_excel("C:\\RAP\\Creative_Industries\\Handicraft-Industrial-Permits\\data\\open\\Handicraft_Industrial_Permits.xlsx")
+
+
+
