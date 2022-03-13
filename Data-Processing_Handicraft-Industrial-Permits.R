57cfa865ee3dd080c15fb524f1a6c69fde0413b9
gbaltor <baltorgabriel@gmail.com>
2022-03-14 02:40:42 +1100

    2.40am Mon 14.03.22
    Data Processing

--- Data-Processing_Handicraft-Industrial-Permits.R
+++ Data-Processing_Handicraft-Industrial-Permits.R
@@ -8,5 +8,15 @@ library("readxl")
 # Extraction: Read Excel
 handicraft_industrial_permits <- read_excel("C:\\RAP\\Creative_Industries\\Handicraft-Industrial-Permits\\data\\open\\Handicraft_Industrial_Permits.xlsx")
 
+category <- unique(handicraft_industrial_permits$CATEGORY)
 
+painting <- dplyr::select(.data = category,
+                          contains("paint"))
 
+painting <- dplyr::select(.data = handicraft_industrial_permits,
+                          contains("paint"))
+
+association <- unique(handicraft_industrial_permits$ASSOCIATION)
+
+library("writexl")
+write.csv(category, "C:\\RAP\\Creative_Industries\\Handicraft-Industrial-Permits\\data\\open\\category.csv")
